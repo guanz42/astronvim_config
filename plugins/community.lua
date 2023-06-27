d342dbca9e2ce98ec4da5d75561aa3d9aac66286
guanz42 <guanz42@163.com>
2023-06-27 16:50:38 +0800

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,11 +5,17 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
 
   -- rust
   { import = "astrocommunity.pack.rust" },
   -- go
   { import = "astrocommunity.pack.go" },
-
+  -- python
+  { import = "astrocommunity.pack.python" },
+  -- typescript
+  { import = "astrocommunity.pack.typescript" },
+  -- html css
+  { import = "astrocommunity.pack.html-css" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
